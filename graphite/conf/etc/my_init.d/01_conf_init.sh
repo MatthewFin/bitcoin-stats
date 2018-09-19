426b3594641bed81172e4bdf9771c5676ef81a00
MatthewFin <matthew.james.finlayson@gmail.com>
2018-09-20 00:56:37 +0200

    clean up

--- graphite/conf/etc/my_init.d/01_conf_init.sh
+++ graphite/conf/etc/my_init.d/01_conf_init.sh
@@ -33,12 +33,3 @@ fi
 if [[ -z $graphite_custom_dir_contents ]]; then
   touch /opt/graphite/webapp/graphite/functions/custom/__init__.py
 fi
-# auto setup statsd with default config if /opt/statsd is missing
-# needed for the use case when a docker host volume is mounted at an of the following:
-#  - /opt/statsd
-statsd_dir_contents=$(find /opt/statsd -mindepth 1 -print -quit)
-if [[ -z $statsd_dir_contents ]]; then
-  git clone -b v0.8.0 https://github.com/etsy/statsd.git /opt/statsd
-  cp $conf_dir/opt/statsd/config_*.js /opt/statsd/
-fi
-
